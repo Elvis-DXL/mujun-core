7cb432d4c7e49ad89ab169b7b68c2e57f0769742
慕君Dxl <dxiaolong@outlook.com>
2025-03-17 16:03:06 +0800

    初始提交

---
+++ oss/src/main/java/com/mujun/core/oss/OssFile.java
@@ -0,0 +1,20 @@
+package com.mujun.core.oss;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Schema(name = "OssFile", description = "对象存储文件上传返回值")
+public class OssFile implements Serializable {
+    @Schema(name = "relUrl", description = "文件相对地址")
+    private String relUrl;
+
+    @Schema(name = "fullUrl", description = "文件全地址")
+    private String fullUrl;
+
+    @Schema(name = "fullName", description = "文件全名称")
+    private String fullName;
+}
